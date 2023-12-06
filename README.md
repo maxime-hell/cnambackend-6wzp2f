2c95dbfc1a9079e02e49acb58e3039c19649bb8e
maxime-hell <95677147+maxime-hell@users.noreply.github.com>
2023-12-06 20:44:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cnambackend-6wzp2f
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/maxime-hell/cnambackend-6wzp2f)
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  backendLoginClient: 'https://cnamdocker.onrender.com/api/utilisateur/login',
+  backendCatalogue: 'https://cnamdocker.onrender.com/api/catalogue',
+};
